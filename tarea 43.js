a66bb99ad05f70d8f8b7cfb5e20a9f1ea5b6d021
Aldohak <88844128+Aldohak@users.noreply.github.com>
2021-12-01 14:10:59 -0600

    Add files via upload

---
+++ tarea 43.js
@@ -0,0 +1,15 @@
+class System{
+
+    constructor(){}
+
+    authenticate(actualCode,enteredCode){
+        textSize(50);
+        fill("black");
+        text(code,300,300)
+        //add code to return true if correct answer entered.
+        if(system.authenticate(accessCode3,this.access3.value()){}
+        
+        
+    }
+
+}
